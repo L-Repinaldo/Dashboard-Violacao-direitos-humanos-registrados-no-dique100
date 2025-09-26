63a54a94fbdd34b623f4a6b94904e9bba0a7b0ca
Lucas Repinaldo <166737770+L-Repinaldo@users.noreply.github.com>
2025-09-26 11:09:31 -0300

    refatoração caminho app

--- Streamlit/Dados/dicionario_caminho_dados.py
+++ Streamlit/Dados/dicionario_caminho_dados.py
@@ -1,10 +1,11 @@
+import os
+
 def dicionario_caminho_dados(pasta):
 
+    base_path = os.path.dirname(os.path.abspath(__file__))  # diretório do script
     caminhos = {
-        "Pasta" : r"Streamlit\Dados\Dados por Grupos Vulneráveis",
+        "Pasta": os.path.join(base_path, "Dados", "Dados por Grupos Vulneráveis")
     }
-
-
     return caminhos[pasta]
 
 
