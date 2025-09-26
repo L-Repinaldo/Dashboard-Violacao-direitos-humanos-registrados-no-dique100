a049bd099d8c8438e8bff4c1b78c84a866e21b34
Lucas Repinaldo <166737770+L-Repinaldo@users.noreply.github.com>
2025-09-26 14:50:55 -0300

    sdfghj

--- Streamlit/Dados/dicionario_caminho_dados.py
+++ Streamlit/Dados/dicionario_caminho_dados.py
@@ -3,6 +3,6 @@ import os
 def dicionario_caminho_dados(pasta):
     base_path = os.path.dirname(os.path.abspath(__file__))
     caminhos = {
-        "Pasta": os.path.join(base_path, "Dados", "Dados por Grupos Vulneráveis")
+        "Pasta": os.path.join(base_path, "Dados por Grupos Vulneráveis")
     }
     return caminhos[pasta]
